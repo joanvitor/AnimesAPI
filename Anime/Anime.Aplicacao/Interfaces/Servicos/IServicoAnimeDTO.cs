721ac497f67f9a2e983bc077dd597d86176dc8f3
Joan Vitor <j.v.m.j@hotmail.com>
2024-05-11 13:45:56 -0300

    - Adicionado os serviços generico para DTO; - Adicionado os serviços do DTO de Anime;

---
+++ Anime/Anime.Aplicacao/Interfaces/Servicos/IServicoAnimeDTO.cs
@@ -0,0 +1,9 @@
+﻿using Anime.Aplicacao.DTOs;
+
+namespace Anime.Aplicacao.Interfaces.Servicos
+{
+    public interface IServicoAnimeDTO : IServicoDTO<Dominio.Entidades.Anime, AnimeDTO>
+    {
+        void RemoverLogicamente(AnimeDTO animeDto);
+    }
+}
---
+++ Anime/Anime.Aplicacao/Servicos/ServicoAnimeDTO.cs
@@ -0,0 +1,26 @@
+﻿using Anime.Aplicacao.Interfaces.Servicos;
+using Anime.Aplicacao.DTOs;
+using Anime.Dominio.Interfaces.Repositorios;
+using AutoMapper;
+
+namespace Anime.Aplicacao.Servicos
+{
+    public class ServicoAnimeDTO : ServicoDTO<Dominio.Entidades.Anime, AnimeDTO>, IServicoAnimeDTO
+    {
+        private readonly IMapper _mapeador;
+        private readonly IRepositorioAnime _repositorio;
+
+        public ServicoAnimeDTO(IMapper mapeador, IRepositorioAnime repositorio) : base(mapeador, repositorio)
+        {
+            _mapeador = mapeador;
+            _repositorio = repositorio;
+        }
+
+        public void RemoverLogicamente(AnimeDTO animeDto)
+        {
+            var entidadeDominio = _mapeador.Map<Dominio.Entidades.Anime>(animeDto);
+
+            _repositorio.RemoverLogicamente(entidadeDominio);
+        }
+    }
+}
