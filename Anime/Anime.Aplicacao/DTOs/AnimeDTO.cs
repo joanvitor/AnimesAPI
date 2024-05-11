5d2666dbf163ba5603a6c93a340e91ed0ef87146
Joan Vitor <j.v.m.j@hotmail.com>
2024-05-11 12:00:26 -0300

    - Criado a camada de aplicação; - Adicionado os DTOs de Anime e Diretor;

---
+++ Anime/Anime.Aplicacao/DTOs/AnimeDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Anime.Aplicacao.DTOs
+{
+    public class AnimeDTO
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Resumo { get; set; }
+        public bool Apagado { get; set; }
+
+        public int DiretorCodigo { get; set; }
+    }
+}
---
+++ Anime/Anime.Aplicacao/DTOs/DiretorDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Anime.Aplicacao.DTOs
+{
+    public class DiretorDTO
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
