11c7a3b3969951e2d4e986d85b7733ff9cb80f93
Joan Vitor <j.v.m.j@hotmail.com>
2024-05-11 13:44:46 -0300

    - Adicionado a interface para CRUD do DTO;

---
+++ Anime/Anime.Aplicacao/Interfaces/OperacoesBancoDeDados/ICRUDDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using Anime.Aplicacao.Interfaces.Marcadores;
+
+namespace Anime.Aplicacao.Interfaces.OperacoesBancoDeDados
+{
+    public interface ICRUDDTO<TEntidade> where TEntidade: IEntidadeDTO
+    {
+        void Cadastrar(TEntidade entidadeDto);
+        IQueryable<TEntidade> Buscar(Expression<Func<TEntidade, bool>> expressaoWhere);
+        IQueryable<TEntidade> BuscarTodos();
+        IQueryable<TEntidade> BuscarPaginado(int numeroDaPagina, int quantidadeEmUmaPagina);
+        void Atualizar(TEntidade entidadeDto);
+        void Excluir(TEntidade entidadeDto);
+        void Salvar();
+    }
+}
