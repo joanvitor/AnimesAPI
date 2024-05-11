3b15a4c0a1497840566d7451dd141da25def7d48
Joan Vitor <j.v.m.j@hotmail.com>
2024-05-11 11:20:51 -0300

    - Criado a interface IRepositorio e sua classe concreta;

---
+++ Anime/Anime.Dominio/Interfaces/Repositorios/IRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using Anime.Dominio.Interfaces.Entidades;
+using Anime.Dominio.Interfaces.OperacaoesBancoDeDados;
+
+namespace Anime.Dominio.Interfaces.Repositorios
+{
+    public interface IRepositorio<TEntidade> : ICRUD<TEntidade> where TEntidade : class, IEntidade
+    {
+    }
+}
