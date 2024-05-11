2beb3298e5c63cb17d575923a36574c6ab370ebb
Joan Vitor <j.v.m.j@hotmail.com>
2024-05-11 11:20:04 -0300

    - Criado interface com as operações básicas em um banco de dados (CRUD);

---
+++ Anime/Anime.Dominio/Interfaces/OperacaoesBancoDeDados/ICRUD.cs
@@ -0,0 +1,16 @@
+﻿using Anime.Dominio.Interfaces.Entidades;
+using System.Linq.Expressions;
+
+namespace Anime.Dominio.Interfaces.OperacaoesBancoDeDados
+{
+    public interface ICRUD<TEntidade> where TEntidade : class, IEntidade
+    {
+        void Cadastrar(TEntidade entidade);
+        IQueryable<TEntidade> Buscar(Expression<Func<TEntidade, bool>> expressaoWhere);
+        IQueryable<TEntidade> BuscarTodos();
+        IQueryable<TEntidade> BuscarPaginado(int numeroDaPagina, int quantidadeEmUmaPagina);
+        void Atualizar(TEntidade entidade);
+        void Excluir(TEntidade entidade);
+        void Salvar();
+    }
+}
