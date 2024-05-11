27f4d163990b040166a0f39c7d359c0fb8fd67b0
Joan Vitor <j.v.m.j@hotmail.com>
2024-05-11 10:46:31 -0300

    - Adicionado as classes Anime e Diretor;

---
+++ Anime/Anime.Dominio/Entidades/Anime.cs
@@ -0,0 +1,13 @@
+﻿namespace Anime.Dominio.Entidades
+{
+    public class Anime
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Resumo { get; set; }
+        public bool Apagado { get; set; }
+
+        public int DiretorCodigo { get; set; }
+        public virtual Diretor Diretor { get; set; }
+    }
+}
---
+++ Anime/Anime.Dominio/Entidades/Diretor.cs
@@ -0,0 +1,10 @@
+﻿namespace Anime.Dominio.Entidades
+{
+    public class Diretor
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+
+        public IEnumerable<Anime> Animes { get; set; }
+    }
+}
