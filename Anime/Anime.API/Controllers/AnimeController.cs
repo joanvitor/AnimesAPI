e81069c301e9de2b86df7d2d44dc786fb71d643a
Joan Vitor <j.v.m.j@hotmail.com>
2024-05-12 12:32:04 -0300

    - Na controller de anime foram feitos as seguintes implementações: - Adicionado métodos: get por código, get paginado, get por codigo do diretor, pelo resumo do anime. - Adicionado também o método para cadastrar (POST), atualizar (PUT) e excluir (Delete);

--- Anime/Anime.API/Controllers/AnimeController.cs
+++ Anime/Anime.API/Controllers/AnimeController.cs
@@ -4,8 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Anime.API.Controllers
 {
+    //TODO dar uma olhada sobre FLuentResult
+
     [ApiController]
-    [Route("api/anime")]
     [Route("api/Anime")]
     public class AnimeController : Controller
     {
@@ -19,9 +20,83 @@ namespace Anime.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<AnimeDTO>> Get()
         {
-            var animes = _servicoAnime.BuscarTodos();
+            var animes = _servicoAnime.ObterAtivos().ToList();
 
             return Ok(animes);
+        }
+
+        [HttpGet]
+        [Route("{codigo:int}")]
+        public ActionResult<AnimeDTO> Get(int codigo)
+        {
+            var animeDto = _servicoAnime.Buscar(codigo);
+
+            if (!animeDto.Apagado)
+                return Ok(animeDto);
+
+            return NotFound();
+        }
+
+        [HttpGet]
+        [Route("{pagina:int}/{quantidade:int}")]
+        public ActionResult<IEnumerable<AnimeDTO>> GetPaginado(int pagina, int quantidade)
+        {
+            var animeDto = _servicoAnime.BuscarPaginado(pagina, quantidade);
+
+            return Ok(animeDto);
+        }
+
+        [HttpGet]
+        [Route("/diretor/{codigoDiretor:int}")]
+        public ActionResult<AnimeDTO> GetByCodigoDiretor(int codigoDiretor)
+        {
+            var animeDto = _servicoAnime.ObterAtivos().Where(x => x.DiretorCodigo == codigoDiretor);
+
+            return Ok(animeDto);
+        }
+
+        [HttpGet]
+        [Route("resumo/{resumo}")]
+        public ActionResult<AnimeDTO> GetByResumo(string resumo)
+        {
+            var animeDto = _servicoAnime.ObterAtivos().Where(x => x.Resumo.Contains(resumo));
+
+            return Ok(animeDto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] AnimeDTO animeDto)
+        {
+            _servicoAnime.Cadastrar(animeDto);
+            _servicoAnime.Salvar();
+
+            return new CreatedAtRouteResult("ObterAnime", new { codigo = animeDto.Codigo }, animeDto);
+        }
+
+
+        [HttpPut("{codigo:int}")]
+        public async Task<ActionResult> Put(int codigo, [FromBody] AnimeDTO animeDto)
+        {
+            if (codigo != animeDto.Codigo)
+            {
+                return BadRequest();
+            }
+
+            _servicoAnime.Atualizar(animeDto);
+            _servicoAnime.Salvar();
+
+            return Ok(animeDto);
+        }
+
+        [HttpDelete("{codigo:int}")]
+        public async Task<ActionResult<AnimeDTO>> Delete(int codigo)
+        {
+            var animeDto = _servicoAnime.Buscar(codigo);
+
+            _servicoAnime.RemoverLogicamente(animeDto);
+            _servicoAnime.Salvar();
+
+            return Ok(animeDto);
         }
     }
 }
