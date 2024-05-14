3f81c3b86ddce535f11f01d7420797c14d9b49b6
Joan Vitor <j.v.m.j@hotmail.com>
2024-05-14 19:50:09 -0300

    - Adicionado chave secreta no arquivo appsettings.json; - Através da interface IConfiguration a chave secreta é obtiva dentro da AutenticacaoController;

--- Anime/Anime.Aplicacao/Interfaces/Servicos/IServicoToken.cs
+++ Anime/Anime.Aplicacao/Interfaces/Servicos/IServicoToken.cs
@@ -1,9 +1,10 @@
 ﻿using Anime.Aplicacao.DTOs;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Anime.Aplicacao.Interfaces.Servicos
 {
     public interface IServicoToken
     {
-        string GerarToken(UsuarioDTO usuario);
+        string GerarToken(UsuarioDTO usuario, SymmetricSecurityKey chave);
     }
 }
